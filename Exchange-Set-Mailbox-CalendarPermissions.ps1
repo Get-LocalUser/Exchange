177ac6566373cbe7a7fa60d198bb676ea175e4cc
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2026-03-24 13:27:26 -0700

    Update Exchange-Set-Mailbox-CalendarPermissions.ps1

--- Exchange-Set-Mailbox-CalendarPermissions.ps1
+++ Exchange-Set-Mailbox-CalendarPermissions.ps1
@@ -12,11 +12,11 @@ param (
     [string]$Permissions
 )
 
-Connect-ExchangeOnline -UserPrincipalName $AdminEmail -ShowBanner:$false -ErrorAction Inquire
+Connect-ExchangeOnline -UserPrincipalName $AdminEmail -ShowBanner:$false -ErrorAction Stop
 
 try {
     Add-MailboxFolderPermission -Identity "$($MailboxEmail):\calendar" -User $User -AccessRights  -ErrorAction Stop
 }
 catch {
-    Write-Host "Failed to add permissions to $MailboxEmail for $User: $($_.Exception.Message)"
+    Write-Host "Failed to add permissions to $MailboxEmail for $User $($_.Exception.Message)"
 }
