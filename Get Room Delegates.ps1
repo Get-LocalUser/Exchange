1d57d465fbb850c311d062f761c764dadb041c77
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2026-08-19 09:18:10 -0700

    .

--- Get Room Delegates.ps1
+++ Get Room Delegates.ps1
@@ -1,8 +1,19 @@
 $rooms = Get-Mailbox -RecipientTypeDetails RoomMailbox
 
 $action = foreach ($room in $rooms) {
-    Get-CalendarProcessing -Identity $room |
-        Select-Object Identity, ResourceDelegates
+    $calProcessing = Get-CalendarProcessing -Identity $room.Name
+    $currentdelegates = $calProcessing.ResourceDelegates
+
+    foreach ($delegate in $currentdelegates) {
+        $delegaterecipient = Get-Recipient -Identity $delegate
+
+        [PSCustomObject]@{
+            Room                = $room.DisplayName
+            RoomSmtpAddress     = $room.PrimarySmtpAddress
+            Delegate            = $delegateRecipient.DisplayName
+            DelegateSmtpAddress = $delegateRecipient.PrimarySmtpAddress
+        }
+    }
 }
 
-$action | Export-Csv -Path "C:\Users\hrkerko\Downloads\RoomDelegates.csv" -NoTypeInformation
+$action | Export-Csv -Path "C:\Users\Admin\Downloads\shared\RoomDelegates.csv" -NoTypeInformation 
